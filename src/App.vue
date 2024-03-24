c300ae796cb14c550ba2c43e1598a6f1f88f9228
paulalextech <psteane123@hotmail.com>
2024-03-24 12:31:47 +0000

    created balance component

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,18 @@
-<template>My App</template>
+<template>
+	<Header />
+	<div class="container">
+		<Balance />
+	</div>
+</template>
 
 <script>
 import Header from './components/Header.vue';
+import Balance from './components/Balance.vue';
+
+export default {
+	components: {
+		Header,
+		Balance,
+	},
+};
 </script>
